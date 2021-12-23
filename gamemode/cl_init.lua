d2c9545e51a9111b889ce5a389b5090aa6969e05
Yell0wAt0m <jjh92625@gmail.com>
2021-12-23 16:49:39 +0000

    Added a client ConVar for player model colour

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -6,3 +6,4 @@ include("derma/cl_menu.lua")
 unity = unity or {}
 
 CreateClientConVar("unity_playermodel", unity.defaultPlayerModels[math.random(#unity.defaultPlayerModels)], true, true, "The players model.")
+CreateClientConVar("unity_playercolor", "0.24 0.34 0.41", true, true, "The players model colour.")
