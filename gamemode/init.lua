66498200a8324a9a6cba4fc1cdce038b7a083a31
Yell0wAt0m <jjh92625@gmail.com>
2021-12-25 16:42:10 +0000

    Cleaned gravity gun drop code

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -276,7 +276,7 @@ concommand.Add("unity_dropweapon", function( client )
 		local weaponClass = weapon:GetClass()
 
 		if GetConVar("unity_givegravitygun"):GetInt() > 0 and weaponClass == "weapon_physcannon" then
-			return false
+			return
 		end
 
 		local entity = ents.Create( weaponClass )
